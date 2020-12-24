b52757bcb6e9def0cba8277f68915a8bb13a73ef
EzerWu <wuxw5@asiainfo.com>
2020-12-24 11:49:20 +0800

    完成‘股票行业排名’的前端页面初步设计

--- project/friday-front-end/src/pages/friday/stockMarket/StockMarketSort.jsx
+++ project/friday-front-end/src/pages/friday/stockMarket/StockMarketSort.jsx
@@ -1,20 +1,101 @@
 import React from 'react';
-import {Tabs} from 'antd'
+import {Table} from 'antd'
+import reqwest from 'reqwest';
 
-const TabPane = Tabs.TabPane;
-const callback = (key) => {
-  console.log(key);
-}
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: true,
+    render: name => `${name.first} ${name.last}`,
+    width: '20%',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    filters: [{ text: 'Male', value: 'male' }, { text: 'Female', value: 'female' }],
+    width: '20%',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+];
 
 export default class ExpensesIndex extends React.Component{
-  render() {
-    return(
-      <Tabs defaultActiveKey="1" onChange={callback}>
-        <TabPane tab="Tab 1" key="1">行业股市</TabPane>
-        <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
-        <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
-      </Tabs>
+  state = {
+    data: [],
+    pagination: {},
+    loading: false,
+  };
 
-    )
+  click(record,rowkey){
+
+    console.log(record);
+
+    console.log(rowkey);
+  }
+
+
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  handleTableChange = (pagination, filters, sorter) => {
+    const pager = { ...this.state.pagination };
+    pager.current = pagination.current;
+    this.setState({
+      pagination: pager,
+    });
+    this.fetch({
+      results: pagination.pageSize,
+      page: pagination.current,
+      sortField: sorter.field,
+      sortOrder: sorter.order,
+      ...filters,
+    });
+  };
+
+  fetch = (params = {}) => {
+    console.log('params:', params);
+    this.setState({ loading: true });
+    reqwest({
+      url: 'https://randomuser.me/api',
+      method: 'get',
+      data: {
+        results: 10,
+        ...params,
+      },
+      type: 'json',
+    }).then(data => {
+      const pagination = { ...this.state.pagination };
+      // Read total count from server
+      // pagination.total = data.totalCount;
+      pagination.total = 200;
+      this.setState({
+        loading: false,
+        data: data.results,
+        pagination,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <Table
+        columns={columns}
+        rowKey={record => record.login.uuid}
+        dataSource={this.state.data}
+        pagination={this.state.pagination}
+        loading={this.state.loading}
+        onChange={this.handleTableChange}
+        onRow={(record,rowkey)=>{
+          return{
+            onClick : this.click.bind(this,record,rowkey)    //点击行 record 指的本行的数据内容，rowkey指的是本行的索引
+          }
+        }}
+      />
+    );
   }
 }
