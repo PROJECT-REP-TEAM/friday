a7c4c488d41471bde9b4438940b2bc0ca43b0576
EzerWu <wuxw5@asiainfo.com>
2020-12-25 14:52:36 +0800

    股市排行数据接口完成

--- project/friday-front-end/src/pages/friday/stockMarket/StockMarketRank.jsx
+++ project/friday-front-end/src/pages/friday/stockMarket/StockMarketRank.jsx
@@ -4,21 +4,43 @@ import reqwest from 'reqwest';
 
 const columns = [
   {
-    title: 'Name',
+    title: '股票代码',
+    dataIndex: 'code',
+    sorter: true,
+  },
+  {
+    title: '股票名称',
     dataIndex: 'name',
     sorter: true,
-    render: name => `${name.first} ${name.last}`,
-    width: '20%',
   },
   {
-    title: 'Gender',
-    dataIndex: 'gender',
-    filters: [{ text: 'Male', value: 'male' }, { text: 'Female', value: 'female' }],
-    width: '20%',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
+    title: '当前价格',
+    dataIndex: 'price',
+    sorter: true,
+  },{
+    title: '涨幅',
+    dataIndex: 'changePercent',
+    sorter: true,
+  },{
+    title: '买入价格',
+    dataIndex: 'buy',
+    sorter: true,
+  },{
+    title: '卖出价格',
+    dataIndex: 'shell',
+    sorter: true,
+  },{
+    title: '最高价格',
+    dataIndex: '最低价格',
+    sorter: true,
+  },{
+    title: '成交量',
+    dataIndex: 'volume',
+    sorter: true,
+  },{
+    title: '成交额',
+    dataIndex: 'turnover',
+    sorter: true,
   },
 ];
 
@@ -47,10 +69,9 @@ export default class ExpensesIndex extends React.Component{
       pagination: pager,
     });
     this.fetch({
-      results: pagination.pageSize,
-      page: pagination.current,
-      sortField: sorter.field,
-      sortOrder: sorter.order,
+      pageSize: pagination.pageSize,
+      pageIndex: pagination.current,
+      sort: sorter.field,
       ...filters,
     });
   };
@@ -59,25 +80,40 @@ export default class ExpensesIndex extends React.Component{
     console.log('params:', params);
     this.setState({ loading: true });
     reqwest({
-      url: 'https://randomuser.me/api',
-      method: 'get',
-      data: {
-        results: 10,
-        ...params,
+      url: 'https://api.doctorxiong.club/v1/stock/rank',
+      method: 'post',
+      contentType: 'application/json',
+      headers:{
+        'Content-Type':'application/json'
       },
+      data: JSON.stringify(
+        {
+          pageSize: 10,
+          sort: 'turnover',
+          ...params,
+        }
+      ),
       type: 'json',
     }).then(data => {
       const pagination = { ...this.state.pagination };
       // Read total count from server
       // pagination.total = data.totalCount;
-      pagination.total = 200;
+      pagination.total = data.data.allPages;
       this.setState({
         loading: false,
-        data: data.results,
+        data: data.data.rank,
         pagination,
       });
     });
   };
+
+
+  click(record,rowkey){
+
+    console.log(record);
+
+    console.log(rowkey);
+  }
 
 
   render() {
@@ -132,11 +168,16 @@ export default class ExpensesIndex extends React.Component{
 
         <Table
           columns={columns}
-          rowKey={record => record.login.uuid}
+          rowKey="code"
           dataSource={this.state.data}
           pagination={this.state.pagination}
           loading={this.state.loading}
           onChange={this.handleTableChange}
+          onRow={(record,rowkey)=>{
+            return{
+              onClick : this.click.bind(this,record,rowkey)    //点击行 record 指的本行的数据内容，rowkey指的是本行的索引
+            }
+          }}
         />
       </div>
     )
